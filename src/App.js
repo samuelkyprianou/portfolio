538114a58519d1d677976ea3f199b7b84aca1e12
samuelkyprianou <samuelkyprianou@gmail.com>
2020-07-18 23:39:53 +0100

    nav

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import './App.css';
+import Nav from'./components/nav'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h3>Sam Kyprianou</h3>
-      </header>
-    </div>
+   <Nav/>
   );
 }
 
---
+++ src/components/nav.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class Nav extends Component {
+    render() {
+        return (
+            <div className="Nav">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Portfolio</li>
+                    <li>Blog</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+        )
+    }
+}
