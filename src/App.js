6e0517f65d0b37849abe6c5090dee2ad30b18db4
samuelkyprianou <samuelkyprianou@gmail.com>
2020-07-22 19:23:18 +0100

    soft scroll

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import './App.css';
 import Nav from'./components/nav'
+import Home from "./components/home"
+import About from "./components/about"
+import Portfolio from "./components/portfolio"
+import Blog from "./components/blog"
+import Contact from "./components/contact" 
 
 function App() {
   return (
+    <div>
    <Nav/>
+   <Home />
+   <About />
+   <Portfolio />
+   <Blog />
+   <Contact />
+   </div>
   );
 }
 
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,17 +1,64 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Link } from "react-scroll";
 
 export default class Nav extends Component {
-    render() {
-        return (
-            <div className="Nav">
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Portfolio</li>
-                    <li>Blog</li>
-                    <li>Contact</li>
-                </ul>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="Nav">
+          <Link
+            activeClass="active"
+            to="home"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={500}
+          > Home
+          </Link >
+          <Link
+            activeClass="active"
+            to="about"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={500}
+          >
+            {" "}
+            About{" "}
+          </Link>
+          <Link
+            activeClass="active"
+            to="portfolio"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={500}
+          >
+            {" "}
+            Portfolio{" "}
+          </Link>
+          <Link
+            activeClass="active"
+            to="blog"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={500}
+          >
+            {" "}
+            Blog{" "}
+          </Link>
+          <Link
+            activeClass="active"
+            to="contact"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={500}
+          >
+            {" "}
+            Contact{" "}
+          </Link>
+      </div>
+    );
+  }
 }
