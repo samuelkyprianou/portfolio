fbb933be159e486f2befba31dae1cf8cebcc2960
samuelkyprianou <samuelkyprianou@gmail.com>
2020-08-04 11:44:58 +0100

    portfolio

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,11 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Container } from "semantic-ui-react";
 
 export default class Portfolio extends Component {
-    render() {
-        return (
-            <div className="portfolio">
-                
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="portfolio">
+        <Container fluid>
+          <div className="title marker">
+            <p>Portfolio</p>
+          </div>
+        </Container>
+      </div>
+    );
+  }
 }
